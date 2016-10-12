8b2a2462ca806b8579be4d619f642c37dc9dde61
Jing Zhang <jingzhang@Jings-MBP-mid-2012.local>
2016-10-11 22:20:21 -0400

    0.1.0 added GSA

--- RBioArray/R/GSA.R
+++ RBioArray/R/GSA.R
@@ -33,14 +33,14 @@ rbio_entrez2geneStats <- function(DEGdfm, cat = "SYMBOL", species = "Hs", pkg = 
 #' @param pVar Gene level p values. Could be, but not exclusive to, a variable of a dataframe.
 #' @param logFCVar Gene level logFC (log fold change). Could be, but not exclusive to, a variable of a dataframe.
 #' @param tVar Gene leve t values. Could be, but not exclusive to, a variable of a dataframe.#'
-#' @return Outputs a \code{list} object with GSA results, both for p value based and t value based.
 #' @details The function is based on piano package. It runs "fisher", "stouffer", "reporter", "tailStrength", "wilcoxon" for p value based GSA, and "page", "gsea", "maxmean" for t value based GSA.
+#' @return Outputs a \code{list} object with GSA results, both for p value based and t value based.
 #' @importFrom piano runGSA
 #' @examples
 #' \dontrun{
 #' GOterm_bp <- loadGSC(file = "c5.bp.v5.0.entrez.gmt", type = "gmt") # load GO term biological process set
 #'
-#' pc_Kegg <- rbioArray_allGSA(GS_Kegg, pVar = pcGSdfm$p_value, logFCVar = pcGSdfm$logFC, tVar = pcGSdfm$t_value)
+#' pc_Kegg <- rbioArray_allGSA(GOterm_bp, pVar = pcGSdfm$p_value, logFCVar = pcGSdfm$logFC, tVar = pcGSdfm$t_value)
 #'
 #' }
 #' @export
@@ -72,4 +72,163 @@ rbioArray_allGSA <- function(MyGS, pVar, logFCVar, tVar){
   fullGS_list <- c(GS_list_p, GS_list_t)
 
   return(fullGS_list)
+}
+
+
+#' @title rbioArray_allGSbox
+#'
+#' @description Generate boxplot from piano GS rank object,
+#' @param GSrank piano GS rank object.
+#' @param GS if GS = \code{"KEGG"}, the function will remove the "KEGG_" string in the GS name variable. Default is \code{"OTHER"}.
+#' @param fileName Output file name.
+#' @param plotWidth Set the width of the plot. Default is \code{170}.
+#' @param plotHeight Set the height of the plot. Default is \code{150}.
+#' @details GSrank takes the resulted object generated from the piano function consensusScores().
+#' @return Outputs a \code{pdf} boxplot figure file with allGSA results.
+#' @importFrom reshape2 melt
+#' @importFrom grid grid.newpage grid.draw
+#' @import ggplot2
+#' @examples
+#' \dontrun{
+#' pcPos_rank_mxdn <- consensusScores(pc_Pos, class = "mixed", direction="down",n = 15, adjusted = TRUE,
+#'                                    method = "median", plot = TRUE, showLegend = F,
+#'                                    rowNames = "names")
+#'
+#' rbioArray_allGSbox(pcPos_rank_mxdn, fileName = "pcPos_rank_mxdn", plotWidth = 260, plotHeight = 240)
+#'
+#' }
+#' @export
+rbioArray_allGSbox <- function(GSrank, GS = "OTHER", fileName,
+                      plotWidth = 170, plotHeight = 150){
+  # prepare the dataframe for ggplot2
+  DFM <- data.frame(GSrank$rankMat)
+  DFM <- DFM[, c(-1, -2)]
+  DFM <- data.frame(t(DFM)) # transpose the dataframe using t()
+  DFM$enrichment <- rownames(DFM)
+  DFM_mlt <- melt(DFM, id.vars = colnames(DFM)[length(colnames(DFM))])
+
+  if (GS == "KEGG"){ # remove "KEGG_" and the space between the terms
+    DFM_mlt$variable <- sapply(DFM_mlt$variable, function(x){
+      i <- substring(x, 6)
+      gsub("_", " ", i)
+    })
+  } else {
+    DFM_mlt$variable <- sapply(DFM_mlt$variable, function(x)gsub("_", " ", x))
+  }
+
+  DFM_mlt$variable <- factor(DFM_mlt$variable, levels = c(unique(DFM_mlt$variable)))
+
+  # plot
+  grid.newpage()
+  plt <- ggplot(DFM_mlt, aes(x = variable, y = value)) + geom_boxplot() +
+    guides(fill = FALSE) + scale_x_discrete(limits = with(DFM_mlt, rev(levels(variable)))) +
+    ylab("rank") +
+    xlab(NULL) +
+    coord_flip() + # flip the axes
+    theme(panel.background = element_rect(fill = 'white', colour = 'black'),
+          panel.border = element_rect(colour = "black", fill = NA, size = 0.5),
+          axis.title = element_text(face = "bold"),
+          legend.position = "bottom")
+
+  # export the file and draw a preview
+  ggsave(filename = paste(fileName,".boxplot.pdf",sep = ""), plot = plt,
+         width = plotWidth, height = plotHeight, units = "mm",dpi = 600)
+  grid.draw(plt) # preview
+}
+
+
+#' @title rbioArray_allGSscatter
+#'
+#' @description Generate scatter plot for piano GS rank heatmap obejct.
+#' @param GSAList GSA list generated from \code{\link{rbioArray_allGSA}}.
+#' @param GS if GS = \code{"KEGG"}, the function will remove the "KEGG_" string in the GS name variable. Default is \code{"OTHER"}.
+#' @param rankCutoff Cutoff value for GS rank line.
+#' @param pCutoff Cutoff value for GS p value line.
+#' @param fileName Output file name.
+#' @param plotHeight Set the height of the plot. Default is \code{150}.
+#' @return Outputs a \code{pdf} scatter figure file with allGSA results.
+#' @details The function is based on piano package.
+#' @importFrom reshape2 melt
+#' @importFrom grid grid.newpage grid.draw
+#' @importFrom piano consensusHeatmap
+#' @import ggplot2
+#' @examples
+#' \dontrun{
+#' pcGObp_Htmap <- consensusHeatmap(pc_GObp, cutoff = 15,method="median",
+#'  colorkey = TRUE, colorgrad = c("blue","white"),
+#'  cellnote = "none")
+#' }
+#' @export
+GSAscatterHT<-function(GSAList, rankCutoff, pCutoff,
+                       fileName, plotWidth = 170, plotHeight = 150){
+  HTmap <-consensusHeatmap(GSAList, cutoff = 15, method = "median",
+                             colorkey = TRUE, colorgrad = c("blue","white"),
+                             cellnote = "none")
+
+  ## data frame prep
+  rank_tmp <- data.frame(HTmap$rankMat)
+  p_value_tmp <- data.frame(HTmap$pMat)
+  names(rank_tmp) <- p_class_name_rank
+  names(p_value_tmp) <- p_class_name_p
+  rank_tmp$GS <- rownames(rank_tmp)
+  p_value_tmp$GS <- rownames(p_value_tmp)
+  rp_tmp <- merge(rank_tmp, p_value_tmp, by="GS",sort=FALSE)
+
+  # subset the total dfm4plot by p value class and merge all the sub dataframes into a new dataframe
+  # with the new variables of "GS", "rank", "p_value", "p_value_class"
+  p_class <- unlist(unique(lapply(strsplit(names(rp_tmp)[2:11], split="_", fixed=TRUE),
+                                  function(x)x[1]))) # get the p class name from the character string from the column names
+
+  for (n in 1:length(p_class)){
+    assign(paste("dfm4plot_", p_class[n], sep = ""), data.frame(rp_tmp[, grep(paste(p_class[n], "+", sep=""),
+                                                                              names(rp_tmp))],
+                                                                GS = rp_tmp$GS, p_class = p_class[n]))
+  } # create temp dataframes for all the p classes
+
+  for (n in 1:length(p_class)){
+    tmpDfm <- get(paste("dfm4plot_", p_class[n], sep = ""))
+    names(tmpDfm)[1:2] <- c("rank", "p_value")
+    assign(paste("dfm4plot_", p_class[n], sep = ""), tmpDfm)
+    rm("tmpDfm")
+  } # change variable names for all the temp data frames
+
+  dfm4plot <- data.frame()
+
+  for (n in 1:length(p_class)){
+    dfm4plot <- rbind(dfm4plot, get(paste("dfm4plot_", p_class[n], sep = "")))
+  } # merge all the temp data frames together
+
+
+  ## ggplotting
+  grid.newpage()
+  ScatterP<-ggplot(dfm4plot, aes(x = p_value, y = rank))+
+    geom_point(aes(shape = factor(p_class)), size=3)+
+    scale_x_continuous(breaks = c(0.1, 0.05, 0),
+                       labels = c("0.1", "0.05", "0"),
+                       expand = c(0, 0),
+                       trans = "reverse", limits = c(0.1, NA))+
+    scale_y_continuous(breaks = c(100, 50, 0),
+                       labels = c("100", "50", "0"),
+                       expand = c(0, 0),
+                       trans = "reverse", limits = c(100, NA))+
+    geom_vline(xintercept = pCutoff)+
+    geom_hline(yintercept = rankCutoff)+
+    labs(x="median p value",y = "consensus score")+
+    theme(panel.background = element_rect(fill = 'white', colour = 'black'),
+          panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+          axis.title = element_text(face = "bold"),
+          legend.position = "bottom",
+          legend.title = element_blank(),
+          legend.background = element_rect(),
+          legend.key = element_blank())+
+    scale_shape_manual(values = c(1:5))
+
+  # export the files and draw a preview
+  write.csv(dfm4plot, file = paste(fileName,".scatterplot.csv",sep = ""))
+  ggsave(filename = paste(fileName,".scatterplot.pdf",sep = ""), plot = ScatterP,
+         width = plotWidth, height = plotHeight, units = "mm",dpi = 600)
+  grid.draw(ScatterP) # preview
+
+  # return the plot dataframe
+  return(dfm4plot)
 }
